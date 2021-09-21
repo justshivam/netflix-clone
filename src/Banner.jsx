921574f66b063a9300f5ce0c7750cf1b67357b43
Shivam Singh <stdnt.shivam@gmail.com>
2021-09-22 04:31:13 +0530

    Implimented API

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Banner.css";
+import axios from "./axios";
+import requests from "./Requests";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+  console.log(movie);
+
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
@@ -9,19 +27,21 @@ function Banner() {
     <header
       className="banner"
       style={{
-        backgroundImage: `url('https://wallpaperaccess.com/full/712390.jpg')`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundSize: "cover",
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.orignal_name}
+        </h1>
         <div className="banner_buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate("This is a test decription", 150)}
+          {truncate(movie?.overview, 150)}
         </h1>
       </div>
 
