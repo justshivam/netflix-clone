0d16f8e4b126e58a24046fb6c95e6cd5bd112b9b
Shivam Singh <stdnt.shivam@gmail.com>
2021-09-22 03:19:22 +0530

    Initial Commit

---
+++ src/Banner.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url('https://wallpaperaccess.com/full/712390.jpg')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">Movie Name</h1>
+        <div className="banner_buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {truncate("This is a test decription", 150)}
+        </h1>
+      </div>
+
+      <div className="banner__fadeBottom" />
+    </header>
+  );
+}
+
+export default Banner;
---
+++ src/HomeScreen.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+import Banner from "./Banner";
+
+function HomeScreen() {
+  return (
+    <div className="homescreen">
+      <Nav />
+      <Banner />
+    </div>
+  );
+}
+
+export default HomeScreen;
