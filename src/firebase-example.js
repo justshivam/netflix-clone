154eff25ea693fe9d2fc3988d8c1e282f24e0d45
Shivam Singh <stdnt.shivam@gmail.com>
2021-09-30 05:56:02 +0530

    Added Firebase-example

---
+++ src/firebase-example.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  // Paste Firebase config here
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+
+export { auth };
+export default db;
